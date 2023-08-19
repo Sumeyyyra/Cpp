816031a392a9f85d7477338c6e4b7b1cefbc0f2b
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-19 16:56:21 +0300

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,18 @@
+//while dongusu
+#include <iostream> //input output kutuphanesi
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"Turkish");//turkçe karakter kullanmamizi saglar.
+
+    int k;
+    while(k<=4)
+    {
+        cout << "*";
+        k++;
+    };
+
+
+}
