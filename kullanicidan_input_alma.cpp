ac3a03f13f019bf31bb15636b36a5382f32d9c47
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-19 12:36:02 +0300

    Create kullanicidan_input_alma.cpp

---
+++ kullanicidan_input_alma.cpp
@@ -0,0 +1,14 @@
+//kullanicidan input alma
+#include <iostream> //input output kutuphanesi
+
+using namespace std;
+
+
+int main()
+{
+    int boy,kilo;
+    cout << "boy ve kilo degerlerinizi girin: "<< endl;
+    cin >> boy >>kilo;
+    cout << kilo;
+    return 0;
+}
