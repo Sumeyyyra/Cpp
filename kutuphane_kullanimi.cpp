20880399e99c0bfed6173cfbbec913e162d44cb7
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-19 12:44:39 +0300

    Create kutuphane_kullanimi.cpp

---
+++ kutuphane_kullanimi.cpp
@@ -0,0 +1,12 @@
+//kutuphane kullanimi
+#include <iostream> //input output kutuphanesi
+#include <cmath> // matematik kutuphanesi round ceil floor
+using namespace std;
+
+int main()
+{
+    int y,x;
+    cin >> x;
+    y=pow(2,x);
+    cout << endl << y;
+}
