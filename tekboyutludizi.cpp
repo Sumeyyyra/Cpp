6a3b9285468283d201bfbd29c3cbaf03ea9df6c1
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-19 20:03:09 +0300

    Create tekboyutludizi.cpp

---
+++ tekboyutludizi.cpp
@@ -0,0 +1,19 @@
+//diziler
+#include <iostream> //input output kutuphanesi
+
+using namespace std;
+
+
+int main()
+{
+    int dizi[4];
+    for(int i=0;i<4;i++)
+    {
+        cin >> dizi[i];
+    }
+    cout <<endl;
+    for(int i=0;i<4;i++)
+    {
+        cout<<" "<<dizi[i];
+    }
+}
