8d01370f1f0ff384b7674ad3fd9f8e8474c53367
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-21 13:22:36 +0300

    Create otomobil.h

---
+++ cppConstructors/otomobil.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Otomobil {
+public:
+    string renk;
+    int beygir ;
+
+    void ruhsatBilgisiGoster();
+    Otomobil(string _renk,int _beygir);
+};
