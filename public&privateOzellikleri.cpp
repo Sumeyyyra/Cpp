ae0e2ed86072b6c02a2a034388ca12ad84005533
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-21 13:28:06 +0300

    Update and rename .orneklercpp to public&privateOzellikleri.cpp

---
+++ public&privateOzellikleri.cpp
@@ -0,0 +1,10 @@
+//public olunca başka fonskiyonlardan nesnelerin özelliklerine erişebiliriz.
+/* public:
+    string renk;
+    int beygir ;
+*/
+//bunlari private yaparsak compile hatası alırız başka fonksiyonlarda kullanmak istediğimiz de 
+/* private:
+    string renk;
+    int beygir ;
+*/
