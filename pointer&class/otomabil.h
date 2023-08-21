79284bb7d6df7fa17d3093103697f75c669f809a
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-21 14:04:48 +0300

    Create otomabil.h

---
+++ pointer&class/otomabil.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Otomobil {
+public:
+    string renk;
+    int beygir ;
+
+    void ruhsatBilgisiGoster();
+    Otomobil(string _renk,int _beygir);
+
+
+};
