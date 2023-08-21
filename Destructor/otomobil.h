f10ac5a5752f7ae4c6bde4b0d4c8a5a455bcbf29
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-21 16:53:04 +0300

    Create otomobil.h

---
+++ Destructor/otomobil.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Otomobil {
+public:
+    //constructor
+    Otomobil(string _renk,int _beygir);
+
+    //Destructor
+    //once header dosyamıza destructorsı yazıyoruz bunu publicde yapıyoruz
+    ~Otomobil();//ben böyle bir şey yazıcam talimatı veriyoruz header dosyasında
+
+    string renk;
+    int beygir ;
+
+
+
+    void ruhsatBilgisiGoster();
+
+
+};
