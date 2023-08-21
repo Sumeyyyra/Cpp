cfa7c3a3022fdfe099f8c80e4042efc5e1516f99
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-21 18:59:11 +0300

    Create hayvan.cpp

---
+++ Inheritance/hayvan.cpp
@@ -0,0 +1,31 @@
+#include "hayvan.h"
+
+Hayvan::Hayvan(string ad){
+    Hayvan::isim=ad;
+
+
+ }
+
+ void Hayvan::beslen()
+ {
+     cout << Hayvan::isim << " besleniyor.." << endl;
+
+ }
+
+
+ void Hayvan::uyu()
+ {
+     cout << Hayvan::isim << " uyuyor.." << endl;
+
+ }
+
+ void Hayvan::setIsim(string ad){
+    Hayvan::isim=ad;
+
+ }
+
+ string Hayvan::getIsim()
+ {
+
+     return Hayvan::isim;
+ }
