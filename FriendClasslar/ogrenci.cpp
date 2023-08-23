0d0bccbf4220acddc8e91ebcbaa47234b8fe3c23
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-08-23 15:05:27 +0300

    Create ogrenci.cpp

---
+++ FriendClasslar/ogrenci.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ogrenci.h"
+
+//constructoru tamamladik.
+using namespace std;
+Ogrenci::Ogrenci(string _ad,int _not1 ,int _not2){
+
+        _not1 = not1;
+        _not2 = not2;
+        _ad = ad;
+
+
+
+}
